de1886fe73560e7493c4605e21a2b257d4f4ac64
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2022-10-16 21:17:20 +0300

    fix code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double Bmi (double weight, double growth) {
-        double g = growth / 100;
-        double result = weight / (g * g);
+        double growthMetr = growth / 100;
+        double result = weight / Math.pow(growthMetr, 2);
         return result;
     }
 }
